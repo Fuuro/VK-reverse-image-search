348eaf68b4ff69beb8823040fe2130b658f1b2d1
Fuuro <66777825+Fuuro@users.noreply.github.com>
2020-11-27 17:15:42 +0300

    Update sauce.py

--- sauce.py
+++ sauce.py
@@ -20,13 +20,16 @@ while True:
                 if atchs:
                     for atch in atchs:
                         if atch['type'] == 'photo':
-                            photo = atch['photo']
+                           photo = atch['photo']
                             url = photo['sizes'][-1]['url']
+                            url=url.split("?")[0]
+                            count=url.count('/')
+                            url2=url.split('/')
+                            url = url.replace(url2[3]+'/', "")
                             img = requests.get(url).content
                             f = io.BytesIO(img)
                             id = event.obj['peer_id']
-                            sauce = ('Saucenao: '+  'https://saucenao.com/search.php?url=' + url + '\n' + '\n' + 'Google: ' + 'https://www.google.com/searchbyimage?&image_url=' + url + '\n' + '\n' + 'Yandex: ' + 'https://yandex.ru/images/search?source=collections&rpt=imageview&url=' + url )
+                            sauce = ('\n\n' + 'Saucenao: '+  'https://saucenao.com/search.php?url=' + url + '\n' + '\n' + 'Google: ' + 'https://www.google.com/searchbyimage?&image_url=' + url + '\n' + '\n' + 'Yandex: ' + 'https://yandex.ru/images/search?source=collections&rpt=imageview&url=' + url + '\n\n' + 'Если ищите аниме по кадру:' + 'https://trace.moe/?mute&url=' + url + "\n\n"  )
                             sender(id, sauce)
-
     except Exception:
         pass
